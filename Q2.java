68f9ebb5141b890aef7eca77a2982247f8b228e6
UTKARSHCHOUDHARY1 <86793634+UTKARSHCHOUDHARY1@users.noreply.github.com>
2022-01-20 22:37:40 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,15 @@
+// Name:- Utkarsh Choudhary
+public class Q2 {
+    public static void main(String args[])  
+{    
+ int a=0,b=1,c,i,count=5;    
+ System.out.print(a+" "+b); 
+ for(i=2;i<count;++i)  
+ {    
+  c=a+b;    
+  System.out.print(" "+c);    
+  a=b;    
+  b=c;    
+ }    
+}
+}
